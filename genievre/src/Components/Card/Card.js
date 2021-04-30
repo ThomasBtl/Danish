1d80e9ba9a91cf1ea7777502219b36ed00727fef
Thomas Bouteille <btltoma@gmail.com>
2021-05-01 01:06:34 +0200

    front-card - Add dragable card component

--- genievre/src/Components/Card/Card.js
+++ genievre/src/Components/Card/Card.js
@@ -1,9 +1,41 @@
+import React, {useState, useRef} from 'react';
 import './Card.css';
 
 export default function Card(){
+    let  divX = 0,  divY = 0, mouseX = 0, mouseY = 0;
+
+    const card = useRef(null);
+
+    function handleMouseDown(e){
+        e.preventDefault();
+
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+
+        card.current.onmouseup=handleMouseUp;
+        card.current.onmousemove=handleMouseMove;
+    }
+    
+    function handleMouseMove(e){
+        e.preventDefault();
+        divX = mouseX - e.clientX;
+        divY = mouseY - e.clientY;
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+
+        card.current.style.top = (card.current.offsetTop - divY) + "px";
+        card.current.style.left = (card.current.offsetLeft - divX) + "px"
+    }
+    
+    function handleMouseUp(e){
+        card.current.onmouseup = null;
+        card.current.onmousemove = null;
+    }
+
     return(
-        <div id="Card">
-            <p>Je suis une carte</p>
+        <div ref={card} onMouseDown={handleMouseDown} className="card">
+            <p>Assets here</p>
         </div>
     )
 }
+
